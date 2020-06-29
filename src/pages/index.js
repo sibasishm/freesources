4999fdb4c3806e1264e9d43d5646cd7e774a927f
Sibasish Mohanty <smsibasish@gmail.com>
2020-06-29 21:04:04 +0530

    learned about getInitialProp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,32 @@
-export default function Index() {
-	return <h1>Hello Next.js!</h1>;
+import Link from 'next/link';
+
+export default function Index({ breeds }) {
+	return (
+		<div>
+			<h1>List of all dog breeds</h1>
+			<div>
+				{breeds.status === 'success' ? (
+					<ol>
+						{Object.keys(breeds.message).map((name, index) => (
+							<li key={index}>
+								<Link as={`/${name}`} href='/[dog]'>
+									<a>{name}</a>
+								</Link>
+							</li>
+						))}
+					</ol>
+				) : (
+					<p>Nothing found.</p>
+				)}
+			</div>
+		</div>
+	);
 }
+
+Index.getInitialProps = async () => {
+	const response = await fetch('https://dog.ceo/api/breeds/list/all');
+	const breeds = await response.json();
+	return {
+		breeds,
+	};
+};
